b87debd701964a6fefa9b7a75fb1fc46df65d5bc
Victoria Mount <victoriamount01@gmail.com>
2020-12-02 17:40:22 -0600

    Complete MVP

--- server.js
+++ server.js
@@ -1,13 +1,26 @@
 const express = require('express');
 
+const usersRouter = require('./users/userRouter')
+
 const server = express();
 
-server.get('/', (req, res) => {
+
+server.use(express.json())
+server.use('/api/users', logger, usersRouter)
+
+
+server.get('/', logger, (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
 //custom middleware
 
-function logger(req, res, next) {}
+function logger(req, res, next) {
+  console.log('Req method: ', req.method)
+  console.log('Req url: ', req.url)
+  console.log('Timestamp: ', new Date())
+
+  next()
+}
 
 module.exports = server;
